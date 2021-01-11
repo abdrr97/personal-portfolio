798b1d44b4680313c2efd393bdbecccb59d377d7
Abdrr97 <abdrr97@gmail.com>
2021-01-11 01:15:04 +0100

    reparing some bugs on javascript

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@ let theme = localStorage.getItem('theme')
 let theme_dots = document.querySelectorAll('.theme-dot')
 let theme_style = document.querySelector('#theme_style')
 
-if (theme == null || theme == undefined) {
-    setTheme('light')
-} else {
-    setTheme(theme)
-}
+init(theme)
+init_buttons(theme)
 
 theme_dots.forEach(theme_dot => {
     theme_dot.addEventListener('click', (event) => {
@@ -30,6 +27,23 @@ function setTheme(mode) {
             theme_style.href = 'themes/purple.css'
             break;
     }
-
     localStorage.setItem('theme', mode)
+    init_buttons(mode)
+}
+function init(_) {
+    if (_ == null || _ == undefined) {
+        setTheme('light')
+    } else {
+        setTheme(_)
+    }
+}
+function init_buttons(_) {
+    theme_dots.forEach(theme_dot => {
+        theme_dot.style.borderWidth = '';
+        let mode = theme_dot.dataset.mode
+        if (mode === _) {
+            theme_dot.style.borderWidth = '5px';
+            return;
+        }
+    })
 }
